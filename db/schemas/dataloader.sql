34cc5c20b951294de7d63f6d8f66ce448f7f2398
Dylan <dohyunk21@gmail.com>
2021-11-30 16:51:43 -0800

    changes to config

--- db/schemas/dataloader.sql
+++ db/schemas/dataloader.sql
@@ -2,11 +2,11 @@
 
 \c qa;
 
-\COPY questions(question_id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness /*answer_id*/) FROM 'db/data/questions.csv' WITH DELIMITER ',' CSV HEADER;
+\COPY questions(question_id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) FROM '../data/questions.csv' WITH DELIMITER ',' CSV HEADER;
 
-\COPY answers(answer_id, question_id, body, date, answerer_name, answerer_email, reported, helpfulness/*, photo_id*/) FROM 'db/data/answers.csv' WITH DELIMITER ',' CSV HEADER;
+\COPY answers(answer_id, question_id, body, date, answerer_name, answerer_email, reported, helpfulness) FROM '../data/answers.csv' WITH DELIMITER ',' CSV HEADER;
 
-\COPY photos(id, answer_id, url) FROM 'db/data/answers_photos.csv' WITH DELIMITER ',' CSV HEADER;
+\COPY photos(id, answer_id, url) FROM '../data/answers_photos.csv' WITH DELIMITER ',' CSV HEADER;
 
 create index idx_product_id on questions(product_id);
 create index idx_answer_id on answers(question_id);
